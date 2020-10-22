2d4b1252b886a92807934151a0f3fa2d0e8ab530
Yan Li <70615457+YanLi-FE@users.noreply.github.com>
2020-10-22 12:19:03 +0300

    Implement "buy" button on painting cards

---
+++ src/components/loader.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg viewBox="0 0 24 24" :width="size" :height="size" class="loader">
+    <circle :r="radius" cx="12" cy="12" fill="transparent" :stroke="strokeColor" :stroke-width="strokeWidth" class="loader__circle"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "loader",
+  props: {
+    size: {
+      type: [Number, String],
+      default: 24,
+    },
+    strokeWidth: {
+      type: [Number, String],
+      default: 1,
+    },
+    strokeColor: {
+      type: String,
+      default: "currentColor",
+    },
+  },
+  computed: {
+    radius() {
+      return 12 - this.strokeWidth;
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.loader {
+  transform-origin: center;
+  transform: rotateZ(-90deg);
+
+  &__circle {
+    stroke-dasharray: 56.553;
+    stroke-dashoffset: 56.553;
+    animation: loader__circle 1500ms ease-in-out infinite;
+  }
+}
+
+@keyframes loader__circle {
+  to {
+    stroke-dashoffset: -56.447;
+  }
+}
+</style>
--- src/components/painting.vue
+++ src/components/painting.vue
@@ -11,7 +11,19 @@
           <div class="painting-for-sale__price-old" v-if="painting.oldPrice">{{ formattedOldPrice }} $</div>
           <div class="painting-for-sale__price">{{ formattedPrice }} $</div>
         </div>
-        <button type="button" class="painting-for-sale__buy-button">Купить</button>
+        <button type="button" class="painting-for-sale__buy-button" :class="isInCartButtonClass" @click="buy"
+                :disabled="isLoading">
+          <template v-if="isLoading">
+            <loader size="36" stroke-width="3"/>
+          </template>
+          <template v-else-if="isInCart">
+            <svg width="20" height="20" fill="none" viewBox="0 0 20 20" class="painting-for-sale__in-cart-checkmark">
+              <path d="M16.5315 4.80937L7.63341 14.237L3.34814 10.1924" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            В корзине
+          </template>
+          <template v-else>Купить</template>
+        </button>
       </div>
       <div class="painting-for-sale__info-bottom painting-for-sale__info-bottom_sold" v-else>
         Продана на аукционе
@@ -21,13 +33,22 @@
 </template>
 
 <script>
+import Loader from "@/components/loader";
+
+const axios = require('axios');
+
 const numberFormatter = new Intl.NumberFormat('ru-RU', {
   useGrouping: true,
   maximumFractionDigits: 0,
 });
 
+function parseCart() {
+  return localStorage.cart ? JSON.parse(localStorage.cart) : [];
+}
+
 export default {
   name: "painting",
+  components: {Loader},
   props: ['painting'],
   computed: {
     paintingAltText() {
@@ -42,6 +63,46 @@ export default {
     formattedOldPrice() {
       return numberFormatter.format(this.painting.oldPrice);
     },
+    isInCartButtonClass() {
+      return this.isInCart ? 'painting-for-sale__buy-button_in-cart' : '';
+    },
+  },
+  data() {
+    return {
+      isLoading: false,
+      isInCart: false,
+    };
+  },
+  methods: {
+    saveCart() {
+      const cart = parseCart();
+      cart.push(this.painting.id);
+      localStorage.cart = JSON.stringify(cart);
+    },
+    buy() {
+      this.isLoading = true;
+      axios.get(`https://jsonplaceholder.typicode.com/posts/1`, {
+        // Forces a new request every time instead of using cache
+        headers: {
+          'Cache-Control': 'no-store',
+          'Pragma': 'no-cache',
+          'Expires': '0',
+        },
+      })
+          .then(() => {
+            this.saveCart();
+            this.isInCart = true;
+          })
+          .catch(() => {
+          })
+          .then(() => this.isLoading = false)
+    },
+  },
+  created() {
+    const cart = parseCart();
+    if (cart.includes(this.painting.id)) {
+      this.isInCart = true;
+    }
   },
 }
 </script>
@@ -94,6 +155,7 @@ export default {
   &__info-bottom {
     display: flex;
     @include mixins.h3;
+
     &_sold {
       padding-bottom: 12px;
     }
@@ -101,6 +163,13 @@ export default {
 
   &__buy-button {
     @include mixins.button;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  &__in-cart-checkmark {
+    margin-right: 7px;
   }
 
   &_sold {
