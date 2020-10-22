5c173816b469a1f97a75479b1f674a9c94379780
Yan Li <70615457+YanLi-FE@users.noreply.github.com>
2020-10-22 15:04:16 +0300

    Add a workaround for IE so last row of flex items is correctly aligned

--- src/components/site-main.vue
+++ src/components/site-main.vue
@@ -3,7 +3,7 @@
     <div class="main-section__width-container">
       <h1 class="main-section__heading">Картины эпохи Возрождения</h1>
       <div class="main-section__paintings">
-        <painting v-for="elem in data" :painting="elem" :key="elem.title + elem.author"/>
+        <painting v-for="(elem, i) in data" :painting="elem" :key="elem.title + elem.author + i"/>
       </div>
     </div>
   </main>
@@ -12,6 +12,19 @@
 <script>
 import Painting from "@/components/painting";
 const data = require('@/assets/data.json');
+
+// because IE doesn't work well with grid
+// this adds invisible data entries to fill the last row
+while(data.length % 4 !== 0) {
+  const copy = {};
+  for(const key in data[0]) {
+    copy[key] = data[0][key];
+  }
+  copy.invisible = true;
+  data.push(copy);
+}
+// /IE
+
 export default {
   name: "site-main",
   components: {Painting},
@@ -32,6 +45,7 @@ export default {
 
   &__width-container {
     @include variables.width-container;
+    margin-bottom: variables.$main-section-padding-bottom;
   }
 
   &__heading {
@@ -39,10 +53,10 @@ export default {
     padding: 45px 0 39px 0;
   }
   &__paintings {
+    /** would prefer to use grid if not for IE */
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    margin-top: -(variables.$painting-margin-top);
   }
 }
 </style>
